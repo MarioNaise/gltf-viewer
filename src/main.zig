9faaa3022fba4812dc1ede05e5b437384bba22ec
MarioNaise <102912402+MarioNaise@users.noreply.github.com>
2026-06-21 09:55:45 +0200

    Parse gltf

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,31 @@
 const std = @import("std");
-const Io = std.Io;
-
-const zgltf = @import("zgltf");
+const Gltf = @import("zgltf");
 
 pub fn main(init: std.process.Init) !void {
     const arena: std.mem.Allocator = init.arena.allocator();
 
     const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+    if (args.len < 2) {
+        std.debug.print("Usage: gltfv [GLTF/GLB-FILE]\n", .{});
+        return;
     }
+
+    const gltf_path = args[1];
+
+    const buf = try std.Io.Dir.cwd().readFileAllocOptions(
+        init.io,
+        gltf_path,
+        arena,
+        .unlimited,
+        .@"4",
+        null,
+    );
+    defer arena.free(buf);
+
+    var gltf = Gltf.init(arena);
+    defer gltf.deinit();
+
+    try gltf.parse(buf);
+
+    gltf.debugPrint();
 }
